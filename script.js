7bc2725c4bb09a53feaaaea928f9867397dba984
Kim Torres <kimberley.5118@gmail.com>
2020-09-25 16:41:30 -0400

    initial index.html, style.css, and script.js, added date to header

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    // display the date in the header using the id and copied the format directly from moment.js
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    
+})
+
