e6cbcb516e83edf0f5ee5053fcb944efe80bba60
Kim Torres <kimberley.5118@gmail.com>
2020-09-26 14:23:05 -0400

    added more classes to my time element, deleted script that I won't be using

--- script.js
+++ script.js
@@ -3,19 +3,6 @@ $(document).ready(function() {
     // display the date in the header using the id and copied the format directly from moment.js
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     
-    //create a div to hold all the time blocks
-    // var timeBlock = $("<div>");
-    // timeBlock.addClass("time-block");
-    // console.log("time block")
-
-    // // array to hold hour options
-    // var hourOptions = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-
-    // for (var i = 0; i<hourOptions.length; i++){
-
-    //     //create time div
-    //     //create text area 
-    //     //create save btn
-    // }
+    var saveBtnEl = $(".saveBtn");
 });
 
